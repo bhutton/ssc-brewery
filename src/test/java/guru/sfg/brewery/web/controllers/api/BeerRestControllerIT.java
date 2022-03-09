d94a904dbdf3ad58afb5bfe3ce30c462666d985a
ben <Lijnfe0912>
2022-03-10 06:54:27 +0800

    Removed filters

--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -4,40 +4,11 @@ import guru.sfg.brewery.web.controllers.BaseIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 public class BeerRestControllerIT extends BaseIT {
-
-    @Test
-    public void deleteBeerCredInUrl() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/ea1f529f-e069-4279-84b6-887b1d0d0d15")
-                        .param("apiKey", "spring").param("apiSecret", "guru"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void deleteBeerCredInUrlBadCreds() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/ea1f529f-e069-4279-84b6-887b1d0d0d15")
-                        .param("apiKey", "spring").header("apiSecret", "guru123"))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    public void deleteBeerBadCreds() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/ea1f529f-e069-4279-84b6-887b1d0d0d15")
-                        .header("Api-Key", "spring").header("Api-Secret", "guruXXXX"))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    public void deleteBeer() throws Exception {
-        mockMvc.perform(delete("/api/v1/beer/ea1f529f-e069-4279-84b6-887b1d0d0d15")
-                        .header("Api-Key", "spring").header("Api-Secret", "guru"))
-                .andExpect(status().isOk());
-    }
 
     @Test
     public void findBeers() throws Exception {
